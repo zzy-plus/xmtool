ca06477490007ec6cc6f47392509935227917a12
zzy77 <15226603631@163.com>
2023-12-28 16:51:24 +0800

    基本功能完成

--- electron/preload.js
+++ electron/preload.js
@@ -2,22 +2,42 @@ const {contextBridge, ipcRenderer, app} = require('electron')
 const fs = require('fs')
 const path = require('path')
 const os = require('os')
+const { execSync } = require('child_process');
 
 
 
 //获取主要路径
 const user_home = os.homedir()
 const ets_path = user_home + '\\Documents\\Euro Truck Simulator 2\\profiles'
-let documents;
 
-fs.readdir(ets_path,(err,files)=>{
-    if(err){
-        console.log('读取文件路径错误')
-        documents = undefined
-        return
+const profiles = fs.readdirSync(ets_path)
+const documents = []
+for (const profile of profiles) {
+    let profile_sii_path = ets_path + '\\' + profile + '\\profile.sii'
+    try {
+        // 在这里执行你的CMD命令
+        execSync(`SII_Decrypt \"${profile_sii_path}\"`);
+    } catch (error) {
+        // 处理错误
+        console.log('Error executing command:', error.message);
     }
-    documents = files
-})
+    let content = fs.readFileSync(profile_sii_path,'utf8')
+    let lines = content.split('\n');
+    for (let i = lines.length - 1; i > 0; i--) {
+        if(lines[i].startsWith(' profile_name')){
+            let item = lines[i].split(':')[1].trim().replace(/"/g,'')
+            let profile_name = decodeURIComponent(item.replace(/\\x/g, "%"));
+            documents.push({
+                profile: profile,
+                profile_name: profile_name
+            })
+            console.log(profile_name)
+            break
+        }
+    }
+
+}
+
 
 
 
